10cbcb95ab9251b5e988ea1b0fc905c0d136cff5
Ariel J <arieljakubowski@gmail.com>
2019-04-25 18:57:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,12 +20,16 @@ return output;
 function currentLine(line) {
   let output;
 if(line.length === 0) {output = "The line is currently empty."}
-else {
-  for( let i = 0; i<line.length; i++) {
-    if(i === 0){
-    output = 'The line is currently: ' + (i+1) +'. ' + line[i];}
-    else{output = 'The line is currently: ' + (i+1) +'. ' + line[i];}}
+else{
+output = 'The line is currently: '
+for(var i = 0; i < line.length ; i++){
+if (i === 0){
+output = output + (i+1) + '. ' + line[i];
+}
+else{
+output = output + ', ' + (i+1) + '. ' + line[i];
   }
+}
 }
 return output;
 }
