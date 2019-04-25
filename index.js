ec5af39c48951ae7d18ccae28358070604176049
Ariel J <arieljakubowski@gmail.com>
2019-04-25 18:19:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let deliLine = [];
 function takeANumber(line, name) {
-place = line.length + 1;
-}
+let place = line.length + 1;
 let greeting = `Welcome, ${name}. You are number ${place} in line.`
 return greeting;
 place.push(name);
+}
 
 
 function nowServing(line) {
-
+if(line.length === 0) {return "There is nobody waiting to be served!"} 
+else { return `Currently serving ${line[0]}.`
+line.shift();
+  
+}
 }
 
 
