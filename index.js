47b6aa47f5d89312518bc03f3d8cb58e4ee5f11d
Ariel J <arieljakubowski@gmail.com>
2019-04-25 18:09:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let deliLine = [];
 function takeANumber(line, name) {
 place = line.length + 1;
 }
-return `Welcome, ${name}. You are number ${place} in line.`
-
+let greeting = `Welcome, ${name}. You are number ${place} in line.`
+return greeting;
 function nowServing(line) {
 
 }
